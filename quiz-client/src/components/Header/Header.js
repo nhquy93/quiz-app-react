8e50b2ec6f3d512de1c62da5f31c11e9476e1883
nhquy93 <nhquy93@gmail.com>
2023-06-06 12:13:07 +0700

    update header layout

--- quiz-client/src/components/Header/Header.js
+++ quiz-client/src/components/Header/Header.js
@@ -1,37 +1,115 @@
 import { Avatar, Input, Typography } from "antd";
 import {
+  ArrowLeftOutlined,
+  ClockCircleOutlined,
   MenuOutlined,
   SearchOutlined,
+  StarFilled,
   SwapOutlined,
   UserOutlined,
 } from "@ant-design/icons";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Header.css";
+import { Link, useLocation } from "react-router-dom";
 
 const { Title, Text } = Typography;
 
-export default function Header() {
+export default function Header(props) {
+  const { pathname } = useLocation();
+
   return (
     <div className="header">
-      <div className="header__menu">
-        <MenuOutlined
-          style={{ fontSize: "24px", color: "#fff", cursor: "pointer" }}
-        />
-        <Avatar size="large" icon={<UserOutlined />} />
-      </div>
-      <div className="header__title">
-        <Text level={3}>Hello, {"James"}</Text>
-        <Title level={4} style={{ margin: 0 }}>
-          Let's test your knowledge
-        </Title>
-      </div>
-      <div className="header__searchbox">
-        <Input
-          placeholder="Search"
-          prefix={<SearchOutlined style={{ color: "#008dff" }} />}
-          suffix={<SwapOutlined style={{ color: "#008dff" }} />}
-        />
-      </div>
+      {pathname === "/" && <HomeHeader username="James" />}
+      {pathname === "/detail" && (
+        <DetailHeader title="UI UX Design" desc="GET 100 Points" rate="4.8" />
+      )}
+      {pathname === "/start" && (
+        <StartHeader title="UI UX Design" timeExpired={900} />
+      )}
     </div>
   );
 }
+
+const HomeHeader = ({ username }) => (
+  <>
+    <div className="header__menu">
+      <MenuOutlined style={{ fontSize: "24px", cursor: "pointer" }} />
+      <Avatar size="large" icon={<UserOutlined />} />
+    </div>
+    <div className="header__title">
+      <Text>Hello, {username}</Text>
+      <Title level={4} style={{ margin: 0 }}>
+        Let's test your knowledge
+      </Title>
+    </div>
+
+    <div className="header__searchbox">
+      <Input
+        placeholder="Search"
+        prefix={<SearchOutlined style={{ color: "#008dff" }} />}
+        suffix={<SwapOutlined style={{ color: "#008dff" }} />}
+      />
+    </div>
+  </>
+);
+
+const DetailHeader = ({ title, desc, rate }) => (
+  <>
+    <div className="header__menu">
+      <span>
+        <Link to={"/"}>
+          <ArrowLeftOutlined style={{ fontSize: "18px", marginRight: 12 }} />
+        </Link>
+        <Text className="header__menu__title">Detail Quiz</Text>
+      </span>
+      <Avatar size="large" icon={<UserOutlined />} />
+    </div>
+    <div className="header__title__detail">
+      <span>
+        <Title level={3} style={{ margin: 0 }}>
+          {title}
+        </Title>
+        <Text type="secondary">{desc}</Text>
+      </span>
+      <span>
+        <StarFilled /> {rate}
+      </span>
+    </div>
+  </>
+);
+
+const StartHeader = ({ title, timeExpired }) => {
+  const [timeLeft, setTimeLeft] = useState(timeExpired);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeLeft]);
+
+  const getReturnValues = (timeLeft) => {
+    const mins = parseInt(timeLeft / 60, 10);
+    const secs = parseInt(timeLeft % 60, 10);
+
+    return (
+      (mins < 10 ? "0" + mins : mins) + ":" + (secs < 10 ? "0" + secs : secs)
+    );
+  };
+
+  return (
+    <>
+      <div className="header__menu">
+        <span>
+          <Link to={"/"}>
+            <ArrowLeftOutlined style={{ fontSize: "18px", marginRight: 12 }} />
+          </Link>
+          <Text className="header__menu__title">{title}</Text>
+        </span>
+        <span className="time-box">
+          <ClockCircleOutlined />
+          {getReturnValues(timeLeft)}
+        </span>
+      </div>
+    </>
+  );
+};
